25965f586ce3aff088a803f00b6e3cbc9d270c18
jeckor <jeckor@mail.ru>
2015-09-26 11:46:33 +0300

    modifided view of program - looking buttons of etalon1

--- laba2/laba2/Form1.Designer.cs
+++ laba2/laba2/Form1.Designer.cs
@@ -32,59 +32,47 @@
             this.etalon2_panel = new System.Windows.Forms.Panel();
             this.etalon3_panel = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // etalon1_panel
             // 
-            this.etalon1_panel.Location = new System.Drawing.Point(15, 18);
+            this.etalon1_panel.Location = new System.Drawing.Point(15, 15);
             this.etalon1_panel.Name = "etalon1_panel";
-            this.etalon1_panel.Size = new System.Drawing.Size(150, 150);
+            this.etalon1_panel.Size = new System.Drawing.Size(170, 170);
             this.etalon1_panel.TabIndex = 0;
             // 
             // etalon2_panel
             // 
-            this.etalon2_panel.Location = new System.Drawing.Point(173, 18);
+            this.etalon2_panel.Location = new System.Drawing.Point(190, 15);
             this.etalon2_panel.Name = "etalon2_panel";
-            this.etalon2_panel.Size = new System.Drawing.Size(150, 150);
+            this.etalon2_panel.Size = new System.Drawing.Size(170, 170);
             this.etalon2_panel.TabIndex = 1;
             // 
             // etalon3_panel
             // 
-            this.etalon3_panel.Location = new System.Drawing.Point(331, 18);
+            this.etalon3_panel.Location = new System.Drawing.Point(366, 15);
             this.etalon3_panel.Name = "etalon3_panel";
-            this.etalon3_panel.Size = new System.Drawing.Size(150, 150);
+            this.etalon3_panel.Size = new System.Drawing.Size(170, 170);
             this.etalon3_panel.TabIndex = 2;
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(519, 18);
+            this.panel1.Location = new System.Drawing.Point(556, 15);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(150, 150);
+            this.panel1.Size = new System.Drawing.Size(170, 170);
             this.panel1.TabIndex = 3;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(89, 237);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(42, 29);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(983, 341);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.etalon3_panel);
             this.Controls.Add(this.etalon2_panel);
             this.Controls.Add(this.etalon1_panel);
             this.Name = "Form1";
             this.Text = "laba2";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -96,7 +84,6 @@
         private System.Windows.Forms.Panel etalon2_panel;
         private System.Windows.Forms.Panel etalon3_panel;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
--- laba2/laba2/Form1.cs
+++ laba2/laba2/Form1.cs
@@ -11,10 +11,10 @@ namespace laba2
 {
     public partial class Form1 : Form
     {
-        System.Windows.Forms.Button[,] etalon1;
-        System.Windows.Forms.Button[,] etalon2;
-        System.Windows.Forms.Button[,] etalon3;
-        System.Windows.Forms.Button[,] user_picture;
+        Button[,] etalon1;
+        Button[,] etalon2;
+        Button[,] etalon3;
+        Button[,] user_picture;
 
         public Form1()
         {
@@ -22,22 +22,47 @@ namespace laba2
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            etalon1 = new Button[15, 15];
-            etalon2 = new Button[15, 15];
-            etalon3 = new Button[15, 15];
-            user_picture = new Button[15, 15];
+            etalon1 = new Button[5, 5];
+            etalon2 = new Button[5, 5];
+            etalon3 = new Button[5, 5];
+            user_picture = new Button[5, 5];
+
+            CreateEtalon(etalon1, etalon1_panel);
+            CreateEtalon(etalon2, etalon2_panel);
+            CreateEtalon(etalon3, etalon3_panel);
 
         }
-        private void CreateEtalon(Button[] etalon, Panel panel)
+        private void CreateEtalon(Button[,] etalon, Panel panel)
         {
             int X = panel.Location.X;
-            int Y = panel.Location.Y;
-            
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    CreateEtalonButton(panel.Name + i * j, X + 30 * i, 15 + 30 * j, (Empty_or_Cell()) ? "#" : "", etalon[i, j], panel);
+        }
+        private void CreateEtalonButton(string name, int x, int y, string text, Button button, Panel panel)
+        {
+            button = new Button();
+            button.Location = new Point(x, y);
+            button.Name = name;
+            button.Size = new Size(25, 25);
+            button.Text = text;
+            button.Click += new EventHandler(Etalon_UserPicture_Click);
+            panel.Controls.Add(button);
+        }
+        private bool Empty_or_Cell()    //if true - cell
+        {
+            Random rnd = new Random();
+            int res = rnd.Next(100);
+            return (res > 50) ? true : false;
         }
 
         private void Etalon_UserPicture_Click(object sender, EventArgs e)
         {
-
+            if (((Button)sender).Text == "#")
+                ((Button)sender).Text = "";
+            else
+                ((Button)sender).Text = "#";
         }
+        
     }
 }
